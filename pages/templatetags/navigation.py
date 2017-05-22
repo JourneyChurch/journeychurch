82bd0fc8d092cef240238cab9bc177775fc4664f
Zac Conant <ZacConant@MacBook-Pro.local>
2017-05-22 11:03:36 -0500

    Navigation template tag is now global to all pages. Added all basic page entries

---
+++ pages/templatetags/navigation.py
@@ -0,0 +1,18 @@
+from django import template
+from django.shortcuts import get_object_or_404
+from pages.models import NavigationMenu
+
+register = template.Library()
+
+# Navigation menu:
+# Custom Tag that can be accessed by {{ navigation }}. Sends NavigationMenu items to navigation.html.
+@register.inclusion_tag("pages/navigation.html")
+def navigation(title='main', inline=True):
+
+    # get navigation menu
+    menu = get_object_or_404(NavigationMenu, title=title)
+
+    # get all of its navigation items
+    navigation_items = menu.navigationitem_set.all().order_by('order')
+
+    return {'navigation_items': navigation_items, 'inline': inline}
--- pages/views.py
+++ pages/views.py
@@ -9,15 +9,8 @@ def index(request, slug=None):
     # Homepage has a slug of home. If there is no slug(/) then get the home page
     if slug is None:
         page = get_object_or_404(Page, slug='home')
-
-    # Otherwise get the page that matches the slug
     else:
         page = get_object_or_404(Page, slug=slug)
-
-    if page.menu == None:
-        navigation_items = None
-    else:
-        navigation_items = page.menu.navigationitem_set.all()
 
     context = {
         'title': page.display_title,
@@ -25,7 +18,7 @@ def index(request, slug=None):
         'link_url': page.link_url,
         'link_text': page.link_text,
         'image': page.image.url,
-        'navigation_items': navigation_items
+        'menu': page.menu
     }
 
     return render(request, 'pages/index.html', context)
