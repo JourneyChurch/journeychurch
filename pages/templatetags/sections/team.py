1ba4670f44e384edfa97b11e8897a564d3c99888
Zac Conant <ZacConant@journey2.journeychurch.tv>
2017-08-02 16:45:38 -0500

    Team: fixed error with section showing closed team

--- pages/templatetags/sections/team.py
+++ pages/templatetags/sections/team.py
@@ -1,16 +1,27 @@
 from django import template
 from django.shortcuts import get_object_or_404
 from pages.models import Content
+from profiles.models import Team
 
 register = template.Library()
 
-# Three Column Section:
+# Team Section:
 # Custom Tag that can be accessed by {% team %}. Creates fields for a three column type section
 @register.inclusion_tag("pages/sections/team.html")
 def team(section):
 
-    # Get profiles from section team
-    profiles = section.team.profile_set.all()
+    # Assume empty initially
+    profiles = None
+
+    # Check if section team exists and is public
+    try:
+        team = Team.objects.get(id=section.team.id)
+    except Team.DoesNotExist:
+        team = None
+
+    # If team exists and is public
+    if team != None:
+        profiles = team.profile_set.all()
 
     context = {
         'profiles': profiles,
